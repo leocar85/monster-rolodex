7d237e483e037e3ca21b3735a0e656fe8a2fad4a
leocar85 <leocar85@gmail.com>
2020-06-27 13:17:27 -0300

    Updates

---
+++ precache-manifest.396f42161ed2455f6baabd112a2b12ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe10112847829f8079845e5d32691b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcc9f7859f043f0d3d1a",
+    "url": "/static/css/main.92f6bd62.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc9f7859f043f0d3d1a",
+    "url": "/static/js/main.ce2f4208.chunk.js"
+  },
+  {
+    "revision": "a8a80d3d96a29d231572",
+    "url": "/static/js/runtime-main.ce297290.js"
+  }
+]);
